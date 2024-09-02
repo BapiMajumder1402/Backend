7cdad7e58a1ff17e20e37b6b2cda4cf875a9fe05
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2024-09-02 11:05:56 +0530

    setup done

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
-app.use((req, res, nxt) => {
-    console.log('middel');
-    nxt();
-})
+const path = require('path')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.render('index')
 })
 app.get('/abc', function (req, res) {
     res.send('abc World')
